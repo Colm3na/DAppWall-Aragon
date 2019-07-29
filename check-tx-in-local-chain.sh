3856a2e59e71f2f70d040d35e6cd8889f6816837
anagutjor <anagutjor@gmail.com>
2019-07-29 22:14:12 +0200

    switch grep for jq

--- check-tx-in-local-chain.sh
+++ check-tx-in-local-chain.sh
@@ -1,3 +1,3 @@
-curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["0x2fbca9a4f782d53cc4ed28006536073b0f093f701d7db5e8bd13896d65e808f4"],"id":1}' http://localhost:8545 | grep topics
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["0x2fbca9a4f782d53cc4ed28006536073b0f093f701d7db5e8bd13896d65e808f4"],"id":1}' http://localhost:8545 |  jq -r ".result.logs[].topics[]"
 
 # source: https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_gettransactionreceipt
