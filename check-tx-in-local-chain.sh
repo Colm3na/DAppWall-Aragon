6dee5aea1ded8630af42632ded4e05a3d1aebd2d
anagutjor <anagutjor@gmail.com>
2019-08-01 18:07:38 +0200

    add some comments

--- check-tx-in-local-chain.sh
+++ check-tx-in-local-chain.sh
@@ -1,4 +1,8 @@
-curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["0xf05d87d5b2b6d20e9b51c95c05e6c3eb26cf7969317e0e2460dce9b840263fe9"],"id":1}' http://localhost:8545 |  jq -r ".result.logs[].topics[]"
-# old one: 0x2fbca9a4f782d53cc4ed28006536073b0f093f701d7db5e8bd13896d65e808f4
+# How I am making tests in local devchain of Aragon:
+#
+# I finally found that doing curl to the localhost:8545 was a great solution.
+# Just update "params" with the tx hash you can find on Metamask while running on the localhost 8545 network.
+
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["0x8de5c6ca81ae8dbb12470e7ab782bcf999868c890d60864ba8d428d268e6f999"],"id":1}' http://localhost:8545 |  jq -r ".result.logs[].topics[]"
 
 # source: https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_gettransactionreceipt
